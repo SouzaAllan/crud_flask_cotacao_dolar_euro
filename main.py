077e5080e01ee7fedabe1c7f37b9fc088be95a53
souzaallan <allanfeitosa@hotmail.com>
2023-11-16 21:39:15 -0300

    fix last inclusions

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def eur(a):
 
 
 
-@app.route('/', methods=["get","post"])
+@app.route('/', methods=["get"])
 def home():
   connection = mysql.connector.connect(
   host="localhost",
@@ -35,57 +35,79 @@ def home():
 
   cursor = connection.cursor()
   
-  info = request.form.get('info')
-  if info is not None and request.form.get['info'] == 'cadastrar':
-    
-    preco = request.form.get['valor']
-    preco = float(preco.replace(',', '.'))
-    sql_insert_Query = "INSERT INTO cotacao.cotacao (price, input_date) VALUES (%s, %s)"
-    
-    data_to_insert = (preco,datetime.now()) 
-    cursor.execute(sql_insert_Query, data_to_insert)
-    connection.commit()
-    sql_select_Query = "select * from cotacao.cotacao order by id desc limit 5"
-    cursor.execute(sql_select_Query)
+
+
+  sql_select_Query = "select * from cotacao.cotacao order by id desc limit 5"
+  cursor.execute(sql_select_Query)
+  if cursor.rowcount == 0:
+    cursor.close()
+    connection.close()
+    return render_template('home.html', tasks="você ainda não possui registros, insira algum")
+  else :
     records = cursor.fetchall()
-    print(records)
-    list_dict = [{'id': item[0],'preco': item[1],'usd': dolar(item[1]),'eur': eur(item[1])} for item in records]
-    # Close the cursor and connection
+    list_dict = [{'id': item[0],'preco': item[1],'usd': dolar(item[1]),'eur':eur(item[1])} for item in records]
+
     cursor.close()
     connection.close()
     return render_template('home.html', tasks=list_dict)
+        
+
+
+@app.route('/process', methods=["POST"])
+def process():
+  connection = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root"
+  )
+
+
+  cursor = connection.cursor()
   
-  elif info is not None and request.form['info'] == 'deletar':
+  if request.form['info'] == 'cadastrar':
+    
+    preco = request.form['valor']
+    preco = float(preco.replace(',', '.'))
+    sql_insert_Query = "INSERT INTO cotacao.cotacao (price, input_date) VALUES (%s, %s)"
+    data_to_insert = (preco,datetime.now()) 
+    cursor.execute(sql_insert_Query, data_to_insert)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return redirect(url_for('home'))
+  
+  elif request.form['info'] == 'deletar':
     id = request.form['id']
     sql_insert_Query = "DELETE FROM cotacao.cotacao where id = %s"
     
     data_to_insert = (id,) 
     cursor.execute(sql_insert_Query, data_to_insert)
     connection.commit()
-    sql_select_Query = "select * from cotacao.cotacao order by id desc limit 5"
-    cursor.execute(sql_select_Query)
-    records = cursor.fetchall()
-    print(records)
-    list_dict = [{'id': item[0],'preco': item[1],'usd': dolar(item[1]),'eur': eur(item[1])} for item in records]
-    # Close the cursor and connection
+    return redirect(url_for('home'))
+  
+  elif request.form['info'] == 'atualizar':
+    id = request.form['id']
+    preco = request.form['valor']
+    preco = float(preco.replace(',', '.'))
+    sql_insert_Query = "UPDATE cotacao.cotacao SET price = %s, input_date = %s WHERE id = %s"
+    data_to_insert = (preco,datetime.now(),id) 
+    cursor.execute(sql_insert_Query, data_to_insert)
+    connection.commit()
     cursor.close()
     connection.close()
-    return render_template('home.html', tasks=list_dict)
+    return redirect(url_for('home'))
+  
 
-  else:
-    sql_select_Query = "select * from cotacao.cotacao order by id desc limit 5"
-    cursor.execute(sql_select_Query)
-    if cursor.rowcount == 0:
-      cursor.close()
-      connection.close()
-      return render_template('home.html', tasks="você ainda não possui registros, insira algum")
-    else :
-      records = cursor.fetchall()
-      list_dict = [{'id': item[0],'preco': item[1],'usd': dolar(item[1]),'eur':eur(item[1])} for item in records]
+@app.route('/update', methods=["POST"])
+def update():
+  id = request.form['id']
+  preco = request.form['preco']
+  preco = float(preco.replace(',', '.'))
+  return render_template('update.html', id=id,preco=preco)
 
-      cursor.close()
-      connection.close()
-      return render_template('home.html', tasks=list_dict)
     
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
